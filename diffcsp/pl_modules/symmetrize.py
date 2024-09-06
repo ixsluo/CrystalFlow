bcbaa1e0518ef7c084be91c7c04d1dea21df1376
ixsluo <xiaoshan.luo@outlook.com>
2024-09-06 20:36:03 +0800

    fix: remove unused rotavg symmetrize
    Signed-off-by: ixsluo <xiaoshan.luo@outlook.com>

--- diffcsp/pl_modules/symmetrize.py
+++ diffcsp/pl_modules/symmetrize.py
@@ -28,24 +28,6 @@ class SymmetrizeRotavg(nn.Module):
         scaled_symmetrized_forces /= num_general_ops.repeat_interleave(num_atoms, dim=0)[:, None]
         return scaled_symmetrized_forces  # 42ms
 
-        scaled_symmetrized_forces = torch.zeros_like(scaled_forces)
-        for iop in range(192):
-            op = general_ops[:, iop, :3, :3].repeat_interleave(num_atoms, dim=0)
-            transformed_forces = torch.einsum('nij,nj->ni', op, scaled_forces)
-            scaled_symmetrized_forces[symm_map[:, iop], :] += transformed_forces
-        scaled_symmetrized_forces /= num_general_ops.repeat_interleave(num_atoms, dim=0)[:, None]
-        return scaled_symmetrized_forces  # 2s
-
-        # scaled_symmetrized_forces = torch.zeros_like(scaled_forces)
-        # na_start = 0
-        # for na, nop, iops, isymm_map in zip(num_atoms, num_general_ops, general_ops.split(num_general_ops.tolist()), symm_map):
-        #     for op, this_op_map in zip(iops, isymm_map):
-        #         transformed_forces = torch.einsum('ij,nj->ni', op[:3, :3], scaled_forces[na_start: na_start + na])
-        #         scaled_symmetrized_forces[[m + na_start for m in this_op_map], :] += transformed_forces
-        #     scaled_symmetrized_forces[na_start: na_start + na] /= nop
-        #     na_start += na
-        # return scaled_symmetrized_forces
-
     def symmetrize_rank1(
         self,
         lattices: torch.Tensor,
