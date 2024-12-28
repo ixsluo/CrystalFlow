d77d6d0ac870d9bab5245276e9b60e569a6b50d8
ixsluo <xiaoshan.luo@outlook.com>
2024-12-28 19:55:36 +0800

    fix: extract gen stript
    Signed-off-by: ixsluo <xiaoshan.luo@outlook.com>

--- scripts/extract_gen.py
+++ scripts/extract_gen.py
@@ -39,12 +39,11 @@ def main(args):
     for pt in args.pt:
         print(f"Extracting {pt}")
         crys_array_list, _ = get_crystal_array_list(pt, batch_idx=mode)
-        for eval_idx, i_crys_array_list in enumerate(crys_array_list):
-            gen_crys = p_map(lambda x: Crystal(x, compute_fp=False), i_crys_array_list, num_cpus=args.njobs)
 
+        if isinstance(crys_array_list[0], dict):  # no nesting
+            gen_crys = p_map(lambda x: Crystal(x, compute_fp=False), crys_array_list, num_cpus=args.njobs)
             strcuture_list = [c.structure if c.constructed else None for c in gen_crys]
-
-            extract_dir = Path(pt).with_suffix(".dir").joinpath(f"{eval_idx}")
+            extract_dir = Path(pt).with_suffix(".dir")
             extract_dir.mkdir(exist_ok=True, parents=True)
             extract_cif_dir = extract_dir / "cif"
             extract_cif_dir.mkdir(exist_ok=True)
@@ -56,6 +55,24 @@ def main(args):
                     Poscar(structure).write_file(extract_vasp_dir / f"{i}.vasp")
                 else:
                     print(f"Error Structure index: {i}.")
+        else:  # multi nesting
+            for eval_idx, i_crys_array_list in enumerate(crys_array_list):
+                gen_crys = p_map(lambda x: Crystal(x, compute_fp=False), i_crys_array_list, num_cpus=args.njobs)
+
+                strcuture_list = [c.structure if c.constructed else None for c in gen_crys]
+
+                extract_dir = Path(pt).with_suffix(".dir").joinpath(f"{eval_idx}")
+                extract_dir.mkdir(exist_ok=True, parents=True)
+                extract_cif_dir = extract_dir / "cif"
+                extract_cif_dir.mkdir(exist_ok=True)
+                extract_vasp_dir = extract_dir / "vasp"
+                extract_vasp_dir.mkdir(exist_ok=True)
+                for i, structure in enumerate(strcuture_list):
+                    if structure is not None:
+                        CifWriter(structure).write_file(extract_cif_dir / f"{i}.cif")
+                        Poscar(structure).write_file(extract_vasp_dir / f"{i}.vasp")
+                    else:
+                        print(f"Error Structure index: {i}.")
 
 
 if __name__ == "__main__":
