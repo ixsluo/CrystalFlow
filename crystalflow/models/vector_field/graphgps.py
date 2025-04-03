0f5318ae44656085c12b8a34d20820f0bbc2ef1d
ixsluo <xiaoshan.luo@outlook.com>
2025-04-03 19:24:53 +0800

    feat: first add GraphGPS
    Signed-off-by: ixsluo <xiaoshan.luo@outlook.com>

--- crystalflow/models/vector_field/graphgps.py
+++ crystalflow/models/vector_field/graphgps.py
@@ -3,6 +3,7 @@ import math
 import torch
 import torch.nn as nn
 from torch_geometric.utils import to_dense_adj, dense_to_sparse
+from torch_geometric.nn import GPSConv, GINEConv
 from torch_scatter import scatter
 
 
@@ -37,18 +38,29 @@ class SinusoidsEmbedding(nn.Module):
         return emb
 
 
-class GPS(nn.Module):
+class GraphGPS(nn.Module):
     def __init__(
         self,
         types_input_dim: tuple[str, int],
         types_output_dim: int | None,
         lattice_input_dim: int,
         lattice_output_dim: int,
-        time_dim,
-        frac_emb_freq,
-        hidden_dim,
+        time_dim: int,
+        frac_emb_freq: int,
+        hidden_dim: int,
+        num_layers: int,
+        edge_style="fc",
+        act_fn: str|nn.Module = "",
+        final_layer_norm: bool = True,
     ):
         super().__init__()
+        self.edge_style = edge_style
+        if isinstance(act_fn, nn.Module):
+            self.act_fn = act_fn
+        elif act_fn.lower() == "silu":
+            self.act_fn = nn.SiLU()
+        else:
+            raise NotImplementedError(f"act_fn: {act_fn} not supported")
 
         if types_input_dim[0].lower() == "linear":
             self.types_smooth = nn.Linear(types_input_dim[1], hidden_dim)
@@ -64,9 +76,37 @@ class GPS(nn.Module):
         self.frac_embedding = SinusoidsEmbedding(frac_emb_freq)
         edge_dim = self.frac_embedding.dim + lattice_input_dim
 
-        self.types_readout = nn.Linear(hidden_dim, types_output_dim) if types_output_dim is not None else nn.Identity()
-        self.lattice_readout = nn.Linear(hidden_dim, lattice_output_dim, bias=False)
-        self.frac_coords_readout = nn.Linear(hidden_dim, 3, bias=False)
+        self.convs = nn.ModuleList(
+            [
+                GraphGPSLayer(channels=hidden_dim, edge_dim=edge_dim, act_fn=self.act_fn)
+                for _ in range(num_layers)
+            ]
+        )
+
+        self.final_layer_norm = nn.LayerNorm(hidden_dim) if final_layer_norm else nn.Identity()
+
+        if types_output_dim is not None:
+            self.types_readout = nn.Sequential(
+                nn.Linear(hidden_dim, hidden_dim // 2),
+                self.act_fn,
+                nn.Linear(hidden_dim // 2, types_output_dim),
+            )
+        else:
+            self.types_readout = nn.Identity()
+        self.lattice_readout = nn.Sequential(
+            nn.Linear(hidden_dim, hidden_dim // 2),
+            self.act_fn,
+            nn.Linear(hidden_dim // 2, hidden_dim // 4),
+            self.act_fn,
+            nn.Linear(hidden_dim // 4, lattice_output_dim, bias=False),
+        )
+        self.frac_coords_readout = nn.Sequential(
+            nn.Linear(hidden_dim, hidden_dim // 2),
+            self.act_fn,
+            nn.Linear(hidden_dim // 2, hidden_dim // 4),
+            self.act_fn,
+            nn.Linear(hidden_dim // 4, 3, bias=False),
+        )
 
     def gen_edges(self, num_atoms, frac_coords, node2graph):
         if self.edge_style == 'fc':
@@ -83,5 +123,45 @@ class GPS(nn.Module):
         edge_features = torch.cat([frac_diff_emb, lattice[edge2graph]], dim=1)
         return edge_features
 
+    def build_node_features(self, t, atom_types, num_atoms):
+        type_emb = self.types_smooth(atom_types)
+        time_emb = self.time_embedding(t).repeat_interleave(num_atoms, dim=0)
+        node_features = torch.cat([type_emb, time_emb], dim=1)
+        node_features = self.node_align(node_features)
+        return node_features
+
     def forward(self, t, num_atoms, atom_types, frac_coords, lattice, node2graph):
-        pass
+        # this atom_types is alread encoded
+        edge_index, frac_diff = self.gen_edges(num_atoms, frac_coords, node2graph)
+        edge2graph = node2graph[edge_index[0]]
+        node_features = self.build_node_features(t, atom_types, num_atoms)
+        edge_features = self.build_edge_features(frac_coords, lattice, edge_index, edge2graph)
+        for conv in self.convs:
+            node_features = conv(node_features, edge_index, edge_features, node2graph)
+        node_features = self.final_layer_norm(node_features)
+        graph_features = scatter(node_features, node2graph, dim=0, reduce='mean')
+
+        types_pred = self.types_readout(node_features)
+        lattice_pred = self.lattice_readout(graph_features)
+        frac_coords_pred = self.frac_coords_readout(node_features)
+        return types_pred, lattice_pred, frac_coords_pred
+
+
+class GraphGPSLayer(nn.Module):
+    def __init__(self, channels, edge_dim, act_fn: nn.Module, attn_kwargs=None):
+        super().__init__()
+        nn_model = nn.Sequential(
+            nn.Linear(channels, channels),
+            act_fn,
+            nn.Linear(channels, channels),
+        )
+        self.conv = GPSConv(
+            channels,
+            GINEConv(nn=nn_model, edge_dim=edge_dim),
+            heads=4,
+            attn_type="multihead",
+            attn_kwargs=attn_kwargs,
+        )
+
+    def forward(self, x, edge_index, edge_attr, batch):
+        return self.conv(x, edge_index=edge_index, edge_attr=edge_attr, batch=batch)
--- crystalflow/models/vfield/equiformer.py
+++
@@ -1,8 +0,0 @@
-import torch
-import torch.nn as nn
-
-
-class EquiformerV2(nn.Module):
-    def __init__(self, *args, **kwargs):
-        super().__init__()
-        self.ln = nn.Linear(3, 3)
